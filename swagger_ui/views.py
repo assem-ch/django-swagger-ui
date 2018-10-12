d5109d5f29e995e7ef09e1b874f337ba920ce6bc
Assem Chelli <assem.ch@gmail.com>
2018-10-12 06:31:45 +0100

    Fix missing template and raise exception when SWAGGER_YAML_FILE is not the user settings

--- swagger_ui/views.py
+++ swagger_ui/views.py
@@ -1,10 +1,13 @@
 import json
 import yaml
+from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import render
-from swagger_viewer.settings import SWAGGER_YAML_FILE
-
+from django.conf import settings
 
 def yaml_to_html(request):
-    file = open(SWAGGER_YAML_FILE)
-    spec = yaml.load(file.read())
-    return render(request, template_name="swagger_base.html", context={'data': json.dumps(spec)})
+    if hasattr(settings, 'SWAGGER_YAML_FILE'):
+        file = open(settings.SWAGGER_YAML_FILE)
+        spec = yaml.load(file.read())
+        return render(request, template_name="swagger_base.html", context={'data': json.dumps(spec)})
+    else:
+        raise ImproperlyConfigured('You should define SWAGGER_YAML_FILE in your settings')
